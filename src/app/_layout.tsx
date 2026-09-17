462705ec78190614824edb2bee7a3edcc44fab7a
Eduarda Navarro da Silva <eduardanavarr.2006@gmail.com>
2026-09-17 08:42:57 -0300

    Projeto AppTarefas 2
    Versão final do App

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,33 @@
-import { Stack } from "expo-router";
+
+//importar o componente que controla a navegação (rotas)
+import{ Stack } from 'expo-router';
 
 export default function Layout( ) {
-  return (
+  return(
     <Stack>
-      <Stack.Screen 
-      name="index" 
-      options={{ 
-        title: "Agendador de Tarefas" ,
-       }}
-       />
+      <Stack.Screen
+      name='index'
+      options={{
+        title: "Agendador de Tarefas",
+      }}
+      />
 
-       <Stack.Screen 
-      name="nova_tarefa" 
-      options={{ 
-        title: "Nova Tarefa" ,
-       }}
-       />
-       
-
+      <Stack.Screen
+      name='nova-tarefa'
+      options={{
+        title: "Nova Tarefa",       
+      }}
+      />
     </Stack>
   );
+  
 }
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useColorScheme } from 'react-native';
+
+import { AnimatedSplashOverlay } from '@/components/animated-icon';
+import AppTabs from '@/components/app-tabs';
+
+SplashScreen.preventAutoHideAsync();
